357eec3398290f757a08ad15b9a020f04845cc53
Bishesh <155248290+0x4ef8@users.noreply.github.com>
2024-06-22 13:03:07 +0545

    Update Send.js

--- utilities/commands/noprefix/Send.js
+++ utilities/commands/noprefix/Send.js
@@ -1,27 +1,49 @@
-module.exports = ({ api, event, userInfo }) => {
+module.exports = ({ api, event, config }) => {
+  let { prefix } = config;
   let input = event.body;
-  let data = input.split("|");
-  if (data.length !== 2) {
+  let args = input.split(" ");
+  
+  if (args.length < 3) {
     api.sendMessage(
-      `⚠️ Invalid use of command!\n💡 Usage: uid|message`,
+      `⚠️Invalid Use Of Command!\n💡Usage: ${prefix}confess [fb url or uid | message]`,
       event.threadID
     );
   } else {
-    let uid = data[0].trim();
-    let message = data[1].trim();
-    
-    // Here you should have a way to map UID to a name or identifier
-    // For example, a database lookup or predefined mapping
-    
-    // Replace the following line with your logic to get user information
-    let userName = getUserInfoFromUID(uid); // Implement this function
-    
-    if (userName) {
-      api.sendMessage(`Sending message "${message}" to ${userName}`, event.threadID);
-      // Assuming you have a way to fetch threadID based on UID, replace threadID with appropriate value
-      api.sendMessage(`${message}`, threadID); // Replace threadID with appropriate value
-    } else {
-      api.sendMessage(`User with UID ${uid} not found.`, event.threadID);
+    try {
+      const y = args.slice(1).join(" ").split("|").map(item => item.trim());
+
+      const t = y[0];
+      const t2 = y[1];
+
+      if (!t) {
+        api.sendMessage("Missing facebook url or uid", event.threadID, event.messageID);
+        return;
+      }
+
+      if (!t2) {
+        api.sendMessage("Missing message", event.threadID, event.messageID);
+        return;
+      }
+
+      if (t.startsWith("https://facebook.com")) {
+        api.getUID(t, (err, res) => {
+          if (err) {
+            api.sendMessage("I'm sorry but your confession has failed to send, I think it's time to chat that person and confess your feelings (⁠◍⁠•⁠ᴗ⁠•⁠◍⁠)", event.threadID, event.messageID);
+          } else {
+            const k = res;
+            api.sendMessage(`󰟫╭ 𝗬𝗼𝘂'𝘃𝗲 𝗴𝗼𝘁 𝗮 𝗺𝗲𝘀𝘀𝗮𝗴𝗲\n\n󰥴 : ${t2}\n━━━━━━━━━━━━━━━━━━━━━\n• :don't bother me to ask who's the sender‚ you're just wasting your time (⁠◍⁠•⁠ᴗ⁠•⁠◍⁠)`, k, () => {
+              api.sendMessage("Confession has been sent successfully!", event.threadID, event.messageID);
+            });
+          }
+        });
+      } else {
+        const k = t;
+        api.sendMessage(`󰟫╭ 𝗬𝗼𝘂'𝘃𝗲 𝗴𝗼𝘁 𝗮 𝗺𝗲𝘀𝘀𝗮𝗴𝗲\n\n󰥴 : ${t2}\n━━━━━━━━━━━━━━━━━━━━━\n• :don't bother me to ask who's the sender‚ you're just wasting your time (⁠◍⁠•⁠ᴗ⁠•⁠◍⁠)`, k, () => {
+          api.sendMessage("Confession has been sent successfully!", event.threadID, event.messageID);
+        });
+      }
+    } catch (err) {
+      api.sendMessage(`Error: ${err}`, event.threadID, event.messageID);
     }
   }
 };
